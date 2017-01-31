ce07a1a878805a9459db3359bd0bd872d522d4e8
Kelvin Mbewe <keokeil@hotmail.com>
2017-01-31 15:53:02 +0100

    initial commit

---
+++ app/components/Navigation.jsx
@@ -0,0 +1,37 @@
+var React = require('react');
+var {Link, IndexLink} = require('react-router');
+
+var Navigation = (props) => {
+    return(
+     <div className='top-bar'>
+      <div className='top-bar-left'>
+        <ul className='menu'>
+          <li className='menu-text'>
+            React Timer App
+          </li>
+          <li>
+            <IndexLink to='/' activeClassName='active-link'>Timer</IndexLink>
+          </li>
+          <li>
+            <Link to='/' activeClassName>Countdown</Link>
+          </li>
+        </ul>
+      </div>
+      <div className='top-bar-right'>
+        <ul className='menu'>
+          <li className='menu-text'>
+            Created by <a href='https://github.com/getwebem' target='_blank'>Kelvin Mbewe</a>
+          </li>
+
+        </ul>
+
+      </div>
+
+     </div>
+
+    );
+
+
+}
+
+module.exports = Navigation;
---
+++ readme.md
@@ -0,0 +1,9 @@
+##ReactTimer App
+#####**Getting started**
+
+ - Run: git clone `https://github.com/getwebem/Jumpstart-Angular-1.x.git`
+ - Run:  `git install`
+ - Run:  `node server.js`
+ - Run:  `webpack -w`
+ - Open:  `localhost:3000`
+ 
